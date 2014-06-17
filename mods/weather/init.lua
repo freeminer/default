b30ca2e8928d074dc3c67c76e10e0b8007deb3a6
proller <proller@github.com>
2014-06-17 14:51:24 +0400

    Update weather

--- mods/weather/init.lua
+++ mods/weather/init.lua
@@ -3,41 +3,41 @@
 -- * snow
 -- * wind (not implemented)
 
-assert(minetest.add_particlespawner, "I told you to run the latest GitHub!")
-assert(minetest.get_heat, "I told you to run the latest GitHub!")
+assert(core.add_particlespawner, "I told you to run the latest GitHub!")
+assert(freeminer.get_heat, "I told you to run the latest freeminer!")
 
 addvectors = function (v1, v2)
 	return {x=v1.x+v2.x, y=v1.y+v2.y, z=v1.z+v2.z}
 end
 
-local cloud_height = tonumber(minetest.setting_get("cloud_height"));
+local cloud_height = tonumber(core.setting_get("cloud_height"));
 
 get_snow = function (p)
 	if not p then return 0 end
 	if p.y > cloud_height then return 0 end
-	local heat = minetest.get_heat(p)
+	local heat = freeminer.get_heat(p)
 	if heat >= 0 then return 0 end
-	local humidity = minetest.get_humidity(p)
+	local humidity = freeminer.get_humidity(p)
 	if humidity < 60 then return 0 end
-	--print('S h='..minetest.get_heat(p)..' h='..minetest.get_humidity(p))
+	--print('S h='..freeminer.get_heat(p)..' h='..freeminer.get_humidity(p))
 	return (humidity-60)/(100-60)
 end
 
 get_rain = function (p)
 	if not p then return 0 end
 	if p.y > cloud_height then return 0 end
-	local heat = minetest.get_heat(p)
+	local heat = freeminer.get_heat(p)
 	if heat <= 0 then return 0 end
 	if heat > 50 then return 0 end
-	local humidity = minetest.get_humidity(p)
+	local humidity = freeminer.get_humidity(p)
 	if humidity < 60 then return 0 end
-	--print('R h='..minetest.get_heat(p)..' h='..minetest.get_humidity(p))
+	--print('R h='..freeminer.get_heat(p)..' h='..freeminer.get_humidity(p))
 	return (humidity-60)/(100-60)
 end
 
-if minetest.setting_getbool("weather") then
-	if minetest.setting_getbool("weather") and minetest.setting_getbool("liquid_real") then
-		dofile(minetest.get_modpath("weather").."/rain.lua")
+if core.setting_getbool("weather") then
+	if core.setting_getbool("weather") and core.setting_getbool("liquid_real") then
+		dofile(core.get_modpath("weather").."/rain.lua")
 	end
-	dofile(minetest.get_modpath("weather").."/snow.lua")
+	dofile(core.get_modpath("weather").."/snow.lua")
 end
--- mods/weather/snow.lua
+++ mods/weather/snow.lua
@@ -1,12 +1,12 @@
 -- Snow
-minetest.register_globalstep(function(dtime)
-	for _, player in ipairs(minetest.get_connected_players()) do
+core.register_globalstep(function(dtime)
+	for _, player in ipairs(core.get_connected_players()) do
 		local ppos = player:getpos()
 		local strength = get_snow(ppos)
-		if strength > 0 and minetest.get_node(ppos).name == "air" then
---print("snow he=".. minetest.get_heat(ppos).." hu=".. minetest.get_humidity(ppos) .. " s=" .. strength)
+		if strength > 0 and core.get_node(ppos).name == "air" then
+--print("snow he=".. freeminer.get_heat(ppos).." hu=".. core.get_humidity(ppos) .. " s=" .. strength)
 		-- Make sure player is not in a cave/house...
-		if minetest.get_node_light(ppos, 0.5) ~= 15 then return end
+		if core.get_node_light(ppos, 0.5) ~= 15 then return end
 
 		local minp = addvectors(ppos, {x=-9, y=7, z=-9})
 		local maxp = addvectors(ppos, {x= 9, y=7, z= 9})
@@ -17,21 +17,29 @@ minetest.register_globalstep(function(dtime)
 		local vel = {x=0, y=   -0.5, z=0}
 		local acc = {x=0, y=   -0.5, z=0}
 
-		minetest.add_particlespawner(5*strength, 0.5,
-			minp, maxp,
-			vel, vel,
-			acc, acc,
-			5, 5,
-			25, 25,
-			false, "weather_snow.png", player:get_player_name())
+		core.add_particlespawner({
+			amount=5*strength, time=0.5,
+			minpos=minp, maxpos=maxp,
+			minvel=vel, maxvel=vel,
+			minacc=acc, maxacc=acc,
+			minexptime=5, maxexptime=5,
+			minsize=25, maxsize=25,
+			collisiondetection=false,
+			texture="weather_snow.png",
+			player=player:get_player_name()
+		})
 
-		minetest.add_particlespawner(4*strength, 0.5,
-			minp_deep, maxp_deep,
-			vel, vel,
-			acc, acc,
-			4, 4,
-			25, 25,
-			false, "weather_snow.png", player:get_player_name())
+		core.add_particlespawner({
+			amount=4*strength, time=0.5,
+			minpos=minp, maxpos=maxp,
+			minvel=vel, maxvel=vel,
+			minacc=acc, maxacc=acc,
+			minexptime=4, maxexptime=4,
+			minsize=25, maxsize=25,
+			collisiondetection=false,
+			texture="weather_snow.png",
+			player=player:get_player_name()
+		})
 		end
 	end
 end)
@@ -44,7 +52,7 @@ local snow_box =
 
 
 -- -[[ Enable this section if you have a very fast PC
-minetest.register_abm({
+core.register_abm({
 	nodenames = {"group:crumbly", "group:snappy", "group:cracky", "group:choppy", "group:melts"},
 	neighbors = {"air"},
 	interval = 10.0, 
@@ -54,22 +62,22 @@ minetest.register_abm({
 	action = function (pos, node, active_object_count, active_object_count_wider)
 		local amount = get_snow(pos)
 		if amount == 0 then return end
-		if minetest.registered_nodes[node.name].drawtype ~= "normal"
-			and minetest.registered_nodes[node.name].drawtype ~= "nodebox"
-			and minetest.registered_nodes[node.name].drawtype ~= "allfaces_optional" then return end
+		if core.registered_nodes[node.name].drawtype ~= "normal"
+			and core.registered_nodes[node.name].drawtype ~= "nodebox"
+			and core.registered_nodes[node.name].drawtype ~= "allfaces_optional" then return end
 		local np = addvectors(pos, {x=0, y=1, z=0})
-		if minetest.get_node_light(np, 0.5) ~= 15 then return end
+		if core.get_node_light(np, 0.5) ~= 15 then return end
 		local addsnow = 1
-		if minetest.get_node(pos).name == "default:snow" then
-			if minetest.add_node_level(pos, 4) > 0 then
-				minetest.set_node(pos, {name="default:ice"})
+		if core.get_node(pos).name == "default:snow" then
+			if core.add_node_level(pos, 4) > 0 then
+				core.set_node(pos, {name="default:ice"})
 			else
 				addsnow = 0
 			end
 		end
-		if addsnow > 0 and minetest.get_node(np).name == "air" then
-			minetest.set_node(np, {name="snow"})
-			minetest.add_node_level(np)
+		if addsnow > 0 and core.get_node(np).name == "air" then
+			core.set_node(np, {name="snow"})
+			core.add_node_level(np)
 		end
 	end
 })
