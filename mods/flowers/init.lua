c58542ffeed6f0c042dfa36c148531508a97a547
alexxvk <alexxvk@yandex.ru>
2015-10-25 05:16:35 +0600

    Fix dry grass and mushrooms 	* dig immediate 	* drop by liquid

--- mods/flowers/init.lua
+++ mods/flowers/init.lua
@@ -191,7 +191,7 @@ for _, m in pairs(mushrooms_datas) do
 		sunlight_propagates = true,
 		walkable = false,
 		buildable_to = true,
-		groups = {snappy = 3, flammable = 3, attached_node = 1},
+		groups = {snappy = 3, flammable = 3, attached_node = 1, dig_immediate = 3, drop_by_liquid = 1},
 		sounds = default.node_sound_leaves_defaults(),
 		on_use = minetest.item_eat(nut),
 		selection_box = {
