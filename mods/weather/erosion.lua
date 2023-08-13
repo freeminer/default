8c9754d578c092b2863b917f11aa803ba8fcc680
proller <proller@github.com>
2023-08-14 01:52:50 +0300

    Erode desert stone

--- mods/weather/erosion.lua
+++ mods/weather/erosion.lua
@@ -8,10 +8,26 @@ core.register_abm({
 	action = function (pos, node, active_object_count, active_object_count_wider)
 		local np = addvectors(pos, {x=0, y=1, z=0})
 		local light = core.get_node_light(np, 0.5)
-    	if not light or light < 13 then return end
-        local top_name = core.get_node(np).name
+		if not light or light < 13 then return end
+		local top_name = core.get_node(np).name
 		if top_name == "default:water_flowing" then
 			core.set_node(pos, {name="default:gravel"})
+		end
+	end
+})
+
+core.register_abm({
+	nodenames = {"default:desert_stone"},
+	neighbors = {"default:water_flowing"},
+	interval = 300.0,
+	chance = 150,
+	action = function (pos, node, active_object_count, active_object_count_wider)
+		local np = addvectors(pos, {x=0, y=1, z=0})
+		local light = core.get_node_light(np, 0.5)
+		if not light or light < 14 then return end
+		local top_name = core.get_node(np).name
+		if top_name == "default:water_flowing" then
+			core.set_node(pos, {name="default:desert_sand"})
 		end
 	end
 })
