bae3a703e28504e49ab9093bfc49d4dbbcd80558
proller <proller@github.com>
2024-02-12 20:22:15 +0100

    Tune grass

--- mods/weather/weather.lua
+++ mods/weather/weather.lua
@@ -138,6 +138,9 @@ core.register_abm({
 		else
 			if node.name == "default:dirt_with_grass" and top_name == "air" and (default.weather and heat > 5 and heat < grass_heat_max and humidity > grass_humidity_min)
 				and (activate or math.random(1, 40) == 1) and light >= grass_light_min then
+
+				if core.find_node_near(pos, (6-5*humidity/100), {"group:flower", "group:tree", "group:sapling"}) then return end
+
 				if rnd1000 <= 10 then
 					set_moonflower(top_pos, "flowers:moonflower_closed")
 				elseif rnd1000 <= 100 then
