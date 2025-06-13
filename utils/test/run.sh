05d5461a41c459031ec4fb76e4bf31f9a2f83256
sfan5 <sfan5@live.de>
2025-06-13 11:52:28 +0200

    Fix Docker CI test to run on new/old engine versions

--- utils/test/run.sh
+++ utils/test/run.sh
@@ -3,6 +3,7 @@ world=$(mktemp -d)
 trap 'rm -rf "$world" || :' EXIT
 
 [ -f game.conf ] || { echo "Must be run in game root folder." >&2; exit 1; }
+[ -n "$DOCKER_IMAGE" ] || { echo "Specify a docker image." >&2; exit 1; }
 
 chmod -R 777 "$world" # container uses unprivileged user inside
 
@@ -12,7 +13,6 @@ vol=(
 	-v "$PWD":/var/lib/minetest/.minetest/games/minetest_game
 	-v "$world":/var/lib/minetest/.minetest/world
 )
-[ -z "$DOCKER_IMAGE" ] && DOCKER_IMAGE="ghcr.io/minetest/minetest:master"
 docker run --rm -i "${vol[@]}" "$DOCKER_IMAGE" --config /etc/minetest/minetest.conf --gameid minetest
 
 test -f "$world/map.sqlite" || exit 1
